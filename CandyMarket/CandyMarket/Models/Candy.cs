be9fe39d107200d41c398978b44e97300d14dd66
Maria <mariabrock414@gmail.com>
2020-05-20 20:51:42 -0500

    added candyclass

---
+++ CandyMarket/CandyMarket/Models/Candy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyMarket.Models
+{
+    public class Candy
+    {
+        public int candyId { get; set; }
+        public string candyName { get; set; }
+        public string manufacturer { get; set; }
+        public string flavorCategory { get; set; }
+
+        public Candy(string CandyName, string Manufacturer, string FlavorCategory)
+        {
+            candyName = CandyName;
+            manufacturer = Manufacturer;
+            flavorCategory = FlavorCategory;
+
+        }
+    }
+}
