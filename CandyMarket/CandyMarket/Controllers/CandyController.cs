e76113b8161ca62e9abfcbdbdccba1165efdb5df
Maria <mariabrock414@gmail.com>
2020-05-20 20:31:02 -0500

    initial setup complete

---
+++ CandyMarket/CandyMarket/Controllers/CandyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CandyMarket.DataAccess;
+
+namespace CandyMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandyController : ControllerBase
+    {
+        CandyRepo _repository;
+
+        public CandyController(CandyRepo repository)
+        {
+            _repository = repository;
+        }
+    }
+}
---
+++ CandyMarket/CandyMarket/DataAccess/CandyRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace CandyMarket.DataAccess
+{
+    public class CandyRepo
+    {
+        string connectionString;
+
+        public CandyRepo(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("CandyMarket");
+        }
+    }
+}
