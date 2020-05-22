9c286d24a5927f47996e87c1f81442174fed495a
Crystal Broach <crysland@gmail.com>
2020-05-22 10:46:36 -0500

    get user candies by id is complete

--- CandyMarket/CandyMarket/DataAccess/CandyRepo.cs
+++ CandyMarket/CandyMarket/DataAccess/CandyRepo.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using CandyMarket.Models;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace CandyMarket.DataAccess
 {
@@ -13,6 +16,45 @@ namespace CandyMarket.DataAccess
         public CandyRepo(IConfiguration config)
         {
             connectionString = config.GetConnectionString("CandyMarket");
+        }
+
+        public IEnumerable<Candy> GetByUserId(int userId)
+        {
+            var sql = @"
+                        select candies.CandyId, candies.CandyName, Candies.Manufacturer, candies.FlavorCategory
+                        from UserCandies
+	                        join Users
+		                        on users.userId = usercandies.userid
+	                        join candies
+		                        on candies.candyid = usercandies.candyid
+                        where usercandies.userid = @userId;
+                      ";
+
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { UserId = userId };
+
+                var candies = db.Query<Candy>(sql, parameters);
+
+                return candies;
+            }
+        }
+
+        public User GetUserById(int userId)
+        {
+            var sql = @"
+                        select *
+                        from users
+                        where userid = @UserId;
+                      ";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { UserId = userId };
+                var user = db.QueryFirstOrDefault<User>(sql, parameters);
+                return user;
+            }
         }
     }
 }
